7ab7904e55c0d8461af94bafff18a13bd8b18a87
MiguelCSenai <cairesmontalvaomiguel@gmail.com>
2024-04-01 21:55:40 -0300

    first commit

--- src/Components/Tabela.js
+++ src/Components/Tabela.js
@@ -1,60 +1,39 @@
 import Table from 'react-bootstrap/Table';
 import "./Tabela.css";
-import Button from 'react-bootstrap/Button';
-import { useState } from "react";
-
-
 function Tabela() {
-    let id_produto_1 = 1;
-    let id_produto_2 = 2;
-    let id_produto_3 = 3;
-    
-    let [qtde_1, setQtd1] = useState (5);
-    let [qtde_2, setQtd2] = useState (4);
-    let [qtde_3, setQtd3] = useState (3);
-    let qtde_soma = (qtde_1+qtde_2+qtde_3);
-    
-    
-    let preco_1 = 10;
-    let preco_2 = 35;
-    let preco_3 = 41;
-    let preco_soma = (preco_1 + preco_2 + preco_3);
-    
-    let total_1 = (preco_1 * qtde_1);
-    let total_2 = (preco_2 * qtde_2);
-    let total_3 = (preco_3 * qtde_3);
-    let total_soma = (total_1+total_2+total_3);
 
-    function BtnQtd1(){
+let produto1 = {
 
-        setQtd1(qtde_1 + 1);
+    id: 1,
+    qtde: 5,
+    preco: 10.00,
+    total: (produto1.preco * produto1.qtde)
 
-    }
-    function BtnQtd2(){
+}
+let produto2 = {
 
-        setQtd2(qtde_2 + 1);
+    id: 2,
+    qtde: 3,
+    preco: 35.00,
+    total: (produto2.preco * produto2.qtde)
 
-    }
-    function BtnQtd3(){
+}
+let produto3 = {
 
-        setQtd3(qtde_3 + 1);
+    id: 3,
+    qtde: 4,
+    preco: 41.00,
+    total: (produto3.preco * produto3.qtde)
 
-    }
-    function BtnSubQtd1(){
+}
+let produtoSomas = {
 
-        setQtd1(qtde_1 - 1);
+    qtde: (produto1.qtde+produto2.qtde+produto3.qtde),
+    preco: (produto1.preco+produto2.preco+produto3.preco),
+    total: (produto1.total+produto2.total+produto3.total)
 
-    }
-    function BtnSubQtd2(){
+}
 
-        setQtd2(qtde_2 - 1);
-
-    }
-    function BtnSubQtd3(){
-
-        setQtd3(qtde_3 - 1);
-
-    }
 
     return (
       <>
@@ -79,52 +58,40 @@ function Tabela() {
           <th class="Qt">Total</th>
         </tr>
         <tr>
-          <th>{id_produto_1}</th>
-          <td>Produto {id_produto_1}</td>
-          <td>Categoria {id_produto_1}</td>
-          <td class="Qt">{qtde_1}</td>
-          <td class="Qt">R$ {preco_1},00</td>
-          <td class="Qt">R$ {total_1},00</td>
+          <th>{produto1.id}</th>
+          <td>Produto {produto1.id}</td>
+          <td>Categoria {produto1.id}</td>
+          <td class="Qt">{produto1.qtde}</td>
+          <td class="Qt">R$ {produto1.preco}</td>
+          <td class="Qt">R$ {produto1.total}</td>
         </tr>
         <tr>
-          <th>{id_produto_2}</th>
-          <td>Produto {id_produto_2}</td>
-          <td>Categoria {id_produto_2}</td>
-          <td class="Qt">{qtde_2}</td>
-          <td class="Qt">R$ {preco_2},00</td>
-          <td class="Qt">R$ {total_2},00</td>
+          <th>{produto2.id}</th>
+          <td>Produto {produto2.id}</td>
+          <td>Categoria {produto2.id}</td>
+          <td class="Qt">{produto2.qtde}</td>
+          <td class="Qt">R$ {produto2.preco}</td>
+          <td class="Qt">R$ {produto2.total}</td>
         </tr>
         <tr>
-          <th>{id_produto_3}</th>
-          <td>Produto {id_produto_3}</td>
-          <td>Categoria {id_produto_3}</td>
-          <td class="Qt">{qtde_3}</td>
-          <td class="Qt">R$ {preco_3},00</td>
-          <td class="Qt">R$ {total_3},00</td>
+          <th>{produto3.id}</th>
+          <td>Produto {produto3.id}</td>
+          <td>Categoria {produto3.id}</td>
+          <td class="Qt">{produto3.qtde}</td>
+          <td class="Qt">R$ {produto3.preco}</td>
+          <td class="Qt">R$ {produto3.total}</td>
         </tr>
 
         <tr>
 
           <td colSpan={3}></td>
-          <td class="Qt">{qtde_soma}</td>
-          <td class="Qt">R$ {preco_soma},00</td>
-          <td class="Qt">R$ {total_soma},00</td>
+          <td class="Qt">{produtoSomas.qtde}</td>
+          <td class="Qt">R$ {produtoSomas.preco}</td>
+          <td class="Qt">R$ {produtoSomas.total}</td>
 
         </tr>
       </tbody>
     </Table>
-
-
-    <div class="containerBtn">
-        <Button variant="secondary" onClick={BtnQtd1}>Aumentar Quantidade do {id_produto_1}° Produto</Button>{' '}
-        <Button variant="secondary" onClick={BtnQtd2}>Aumentar Quantidade do {id_produto_2}° Produto</Button>{' '}
-        <Button variant="secondary" onClick={BtnQtd3}>Aumentar Quantidade do {id_produto_3}° Produto</Button>{' '}
-        <br/><br/>
-        <Button variant="secondary" onClick={BtnSubQtd1}>Diminuir Quantidade do {id_produto_1}° Produto</Button>{' '}
-        <Button variant="secondary" onClick={BtnSubQtd2}>Diminuir Quantidade do {id_produto_2}° Produto</Button>{' '}
-        <Button variant="secondary" onClick={BtnSubQtd3}>Diminuir Quantidade do {id_produto_3}° Produto</Button>{' '}
-
-    </div>
 
     </div>
         
